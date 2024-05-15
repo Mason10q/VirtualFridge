d3b9d538b6e706216774ba05cde393c4342de53a
mason10q <moskvichev610@gmail.com>
2024-05-16 00:46:35 +0300

    Added module for resources

---
+++ core-res/build.gradle.kts
@@ -0,0 +1,43 @@
+import buildsrc.Libs
+import buildsrc.Versions
+
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "ru.virtual.core_res"
+    compileSdk = Versions.compileSdk
+
+    defaultConfig {
+        minSdk = Versions.minSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = Versions.compatibility
+        targetCompatibility = Versions.compatibility
+    }
+    kotlinOptions {
+        jvmTarget = Versions.jvmTarget
+    }
+}
+
+dependencies {
+
+    implementation(Libs.AndroidX.core)
+    implementation(Libs.AndroidX.appCompat)
+    implementation(Libs.Google.material)
+}
